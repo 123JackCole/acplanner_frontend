441906c1b532b72b9d040cb2aa405e1e20a3483a
123JackCole <123Jack.Cole@gmail.com>
2023-04-28 15:57:24 -0700

    Updates

--- precache-manifest.51aa713509975377f7de406b761df99c.js
+++ precache-manifest.51aa713509975377f7de406b761df99c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c40b8871f115fa318d0e2dcb6d13f5e",
+    "revision": "3bb3d2f3e1545a77b8e1ac2643249ce6",
     "url": "/acplanner_frontend/index.html"
   },
   {
-    "revision": "939e90b90285e14d1dc0",
+    "revision": "3d9bbdca5dcd821c66a0",
     "url": "/acplanner_frontend/static/css/main.1dc18626.chunk.css"
   },
   {
-    "revision": "ba0ec24f721474a0b21b",
-    "url": "/acplanner_frontend/static/js/2.a72da6c1.chunk.js"
+    "revision": "f72ded5299ba59c7ed93",
+    "url": "/acplanner_frontend/static/js/2.81453ae2.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/acplanner_frontend/static/js/2.a72da6c1.chunk.js.LICENSE.txt"
+    "url": "/acplanner_frontend/static/js/2.81453ae2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "939e90b90285e14d1dc0",
-    "url": "/acplanner_frontend/static/js/main.ab67baaf.chunk.js"
+    "revision": "3d9bbdca5dcd821c66a0",
+    "url": "/acplanner_frontend/static/js/main.84d01ced.chunk.js"
   },
   {
     "revision": "677cb64345a0a1216dc3",
