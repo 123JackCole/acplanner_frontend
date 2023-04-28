4ffc63e5ddfc8527dbe77d721a8996d92ffe027d
123JackCole <123Jack.Cole@gmail.com>
2023-04-28 14:28:46 -0700

    ran npm run deploy

---
+++ build/precache-manifest.56e59b549780835d1c71db20002a30b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76edc7ec574471979da5352a23068a39",
+    "url": "/acplanner_frontend/index.html"
+  },
+  {
+    "revision": "0eb2ed4b54a0f20535a7",
+    "url": "/acplanner_frontend/static/css/main.1dc18626.chunk.css"
+  },
+  {
+    "revision": "262f833501c926b4c610",
+    "url": "/acplanner_frontend/static/js/2.a5908e16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/acplanner_frontend/static/js/2.a5908e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb2ed4b54a0f20535a7",
+    "url": "/acplanner_frontend/static/js/main.320f73a6.chunk.js"
+  },
+  {
+    "revision": "677cb64345a0a1216dc3",
+    "url": "/acplanner_frontend/static/js/runtime-main.cef0de8d.js"
+  }
+]);
